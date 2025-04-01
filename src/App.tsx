cb48304f6306950365a9ab7507ffb73183742779
Shriya N <192004shriya@gmail.com>
2025-04-01 20:41:28 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import NotFound from "./pages/NotFound";
+import Predictor from "./pages/Predictor";
+import Dashboard from "./pages/Dashboard";
+import Guide from "./pages/Guide";
+import About from "./pages/About";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/predictor" element={<Predictor />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/about" element={<About />} />
+          
+          {/* Catch-all route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
